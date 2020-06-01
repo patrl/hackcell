fcc4dd1f9e03272f6b833f933ecb5b7eeafe7cf4
Patrick Elliott <patrick.d.elliott@gmail.com>
2020-06-01 13:28:59 -0400

    simple sdl2 test

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,28 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
+import qualified SDL
+import SDL.Vect
+import           Control.Concurrent             ( threadDelay )
+
+screenWidth = 800
+screenHeight = 600
+
 main :: IO ()
-main = print "hello haskell!"
+main = do
+  SDL.initialize [SDL.InitVideo]
+
+  window <- SDL.createWindow
+    "SDL Tutorial"
+    SDL.defaultWindow { SDL.windowInitialSize = V2 screenWidth screenHeight }
+
+  screenSurface <- SDL.getWindowSurface window
+  let white = V4 maxBound maxBound maxBound maxBound
+  SDL.surfaceFillRect screenSurface Nothing white
+  SDL.updateWindowSurface window
+
+  threadDelay 2000000
+
+  SDL.destroyWindow window
+  SDL.quit
